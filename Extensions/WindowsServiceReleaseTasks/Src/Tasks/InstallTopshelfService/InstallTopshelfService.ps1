7e1a5edb2e47ac2bb07aa29e68d1ed3fc8c72e70
Michael Barry <michael.barry@gmo.com>
2018-05-08 16:52:45 -0400

    GMSA requires empty password to be sent

--- Extensions/WindowsServiceReleaseTasks/Src/Tasks/InstallTopshelfService/InstallTopshelfService.ps1
+++ Extensions/WindowsServiceReleaseTasks/Src/Tasks/InstallTopshelfService/InstallTopshelfService.ps1
@@ -35,9 +35,9 @@ Try {
         
         Write-Host ("##vso[task.setvariable variable=E34A69771F47424D9217F3A4D6BCDC95;issecret=true;]$servicePassword")  # Make sure the password doesn't show up in the log.
         
-        $additionalSharedArguments += " -username ""$serviceUsername"""
+        $additionalSharedArguments += " -username ""$serviceUsername"" -password"
         if (-Not [string]::IsNullOrWhiteSpace($servicePassword)) {
-            $additionalSharedArguments += " -password:""$servicePassword"""
+            $additionalSharedArguments += ":""$servicePassword"""
         }
     }
     else {
